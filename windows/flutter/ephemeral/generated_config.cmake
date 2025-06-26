63904b884bf68f00caa01387a07af55e3558eca1
jbarbosa0 <joaovittorandre@hotmail.com>
2025-06-26 10:38:18 -0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\joaov\\OneDrive\\Área de Trabalho\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\joaov\\OneDrive\\Área de Trabalho\\Nova pasta (3)\\namer_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\joaov\\OneDrive\\Área de Trabalho\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\joaov\\OneDrive\\Área de Trabalho\\Nova pasta (3)\\namer_app"
+  "FLUTTER_ROOT=C:\\Users\\joaov\\OneDrive\\Área de Trabalho\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\joaov\\OneDrive\\Área de Trabalho\\Nova pasta (3)\\namer_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\joaov\\OneDrive\\Área de Trabalho\\Nova pasta (3)\\namer_app"
+  "FLUTTER_TARGET=C:\\Users\\joaov\\OneDrive\\Área de Trabalho\\Nova pasta (3)\\namer_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZmNmMmMxMTU3Mg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZGQ5M2RlNmZiMQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\joaov\\OneDrive\\Área de Trabalho\\Nova pasta (3)\\namer_app\\.dart_tool\\package_config.json"
+)
